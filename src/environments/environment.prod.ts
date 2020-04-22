e5a15660855866656519e0f043c4bca78af195b9
Alex Elam <aelam@grimco.com>
2020-04-22 17:23:34 -0500

    Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  API_HOST: "https://loan-calculator-api-staging.azurewebsites.net/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_HOST: "https://loan-calculator-api-staging.azurewebsites.net/",
+  GOOGLE_CLIENT_ID: "395522895595-mg2923mps4fm0jlb9p9j3ag0hsfrrvlg.apps.googleusercontent.com",
+  FACEBOOK_APP_ID: "649424762459082"
 };
 
 /*
