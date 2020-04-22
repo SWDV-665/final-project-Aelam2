14a56075a46b04deec02b36a8a4f751a99cec32a
Alex Elam <aelam@grimco.com>
2020-04-22 17:22:28 -0500

    Payment Schedule INIT

---
+++ src/app/pages/authorized/payment-schedule/payment-schedule.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-schedule',
+  templateUrl: './payment-schedule.page.html',
+  styleUrls: ['./payment-schedule.page.scss'],
+})
+export class PaymentSchedulePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
