96ecb5df906ca51a23a2ede6d04f84c66b841e77
Alex Elam <aelam@grimco.com>
2020-04-29 05:33:28 -0500

    Dashboard Icons and Fetch Logic

--- src/app/pages/authorized/dashboard/dashboard.page.ts
+++ src/app/pages/authorized/dashboard/dashboard.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { LoansService, LoanInfo } from "src/app/services/loans/loans.service";
+import { PaymentPlansService, PaymentPlan } from "src/app/services/paymentPlans/payment-plans.service";
 
 @Component({
   selector: "app-overview",
@@ -6,7 +8,36 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./dashboard.page.scss"]
 })
 export class DashboardPage implements OnInit {
-  constructor() {}
+  public Loans: LoanInfo[] = [];
+  public plans: PaymentPlan[] = [];
+
+  constructor(public LoanService: LoansService, private plansService: PaymentPlansService) {
+    this.LoanService.dataChanged$.subscribe((dataChanged: boolean) => {
+      this.getLoans();
+    });
+
+    this.plansService.dataChanged$.subscribe((dataChanged: boolean) => {
+      this.getPaymentPlans();
+    });
+  }
 
   ngOnInit() {}
+
+  getLoans = () => {
+    this.LoanService.fetchLoans().subscribe(
+      (loans: LoanInfo[]) => {
+        this.Loans = loans;
+      },
+      error => {}
+    );
+  };
+
+  getPaymentPlans = () => {
+    this.plansService.fetchPaymentPlans().subscribe(
+      (plans: PaymentPlan[]) => {
+        this.plans = plans;
+      },
+      error => {}
+    );
+  };
 }
